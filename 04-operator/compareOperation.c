2e5ae7f73e6b682f30287b3dd57aec0af56d78a9
sam <belonk@126.com>
2020-10-20 11:03:02 +0800

    add operator test file.

---
+++ 04-operator/compareOperation.c
@@ -0,0 +1,15 @@
+//
+// Created by Sam Sune on 2020/10/20.
+//
+#include <stdio.h>
+
+int main() {
+    int a = 8;
+    int b = 7;
+    printf("\n a>b=%d", a > b); // 1
+    printf("\n a>=b=%d", a >= b); // 1
+    printf("\n a<b=%d", a < b); //0
+    printf("\n a<=b=%d", a <= b);//0
+    printf("\n a==b=%d", a == b);//0
+    printf("\n a!=b=%d", a != b);//1
+}
